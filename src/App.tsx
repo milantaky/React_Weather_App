4191783c09b0e7d4fa170c894f042dbe94c44e9c
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2024-12-28 20:45:09 +0100

    Added cities to list from (API accepts only long, lat, not city name)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+import { cities } from './cities.tsx';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [location, setLocation] = useState("");
+  // useEffect(() => {
+  //   let ignore = false;
+
+  //   if(!ignore){
+  //     const data = fetch()
+  //   }
+
+  //   return () => ignore = true;
+  // }, [location]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Weather App</h1>
+      <input 
+        value={location}
+        onChange={e => {
+          setLocation(e.target.value);
+        }}    
+      />
+      {cities.map(city => {
+        return <div>{city.name}</div>
+      })}
     </>
   )
 }
 
-export default App
---
+++ src/cities.tsx
@@ -0,0 +1,14 @@
+export const cities = [
+    { name: "London", long: "-0.1257", lat: "51.5085" },
+    { name: "New York", long: "-74.0060", lat: "40.7128" },
+    { name: "Tokyo", long: "139.6917", lat: "35.6895" },
+    { name: "Sydney", long: "151.2093", lat: "-33.8688" },
+    { name: "Paris", long: "2.3522", lat: "48.8566" },
+    { name: "Moscow", long: "37.6173", lat: "55.7558" },
+    { name: "Cape Town", long: "18.4241", lat: "-33.9249" },
+    { name: "Beijing", long: "116.4074", lat: "39.9042" },
+    { name: "Mumbai", long: "72.8777", lat: "19.0760" },
+    { name: "Rio de Janeiro", long: "-43.1729", lat: "-22.9068" },
+    { name: "Prague", long: "14.4378", lat: "50.0755" },
+    { name: "Brno", long: "16.6068", lat: "49.1951" }
+];
