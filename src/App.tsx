2ed365499094c4a1130431ca8aac72776c284fe6
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2025-01-02 14:16:55 +0100

    Changed from searchbar to selection, fetching weather data

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,38 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { cities } from './cities.tsx';
 
+// type LocationData = {
+//   name: string;
+//   lat: number;
+//   long: number;
+// };
+
 export default function App() {
-  const [location, setLocation] = useState("");
-  // useEffect(() => {
-  //   let ignore = false;
+  const [location, setLocation] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
+  const url = location === null ? "" : `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.long}&appid=${import.meta.env.VITE_WEATHER_APP_API_KEY}`;
 
-  //   if(!ignore){
-  //     const data = fetch()
-  //   }
+  useEffect(() => {
+    if(location){
+      fetch(url)
+      .then(response => response.json())
+      .then(data => setWeatherData(data))
+    };
 
-  //   return () => ignore = true;
-  // }, [location]);
+  }, [location]);
 
   return (
     <>
       <h1>Weather App</h1>
-      <input 
-        value={location}
-        onChange={e => {
-          setLocation(e.target.value);
-        }}    
-      />
-      {cities.map(city => {
-        return <div>{city.name}</div>
-      })}
+      <select onChange={e => {
+        const selectedCity = cities.find(city => city.name === e.target.value);
+        setLocation(selectedCity);
+        }}>
+        {cities.map(city => {
+          return <option key={city.name} value={city.name}>{city.name}</option>
+          })}
+      </select>
+      {console.log(weatherData)}
     </>
   )
 }
